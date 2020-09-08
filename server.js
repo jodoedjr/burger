388605a9c4c6d0495410fd2d6eeda50cb52304c6
John Edwards <jdedwards6789@gmail.com>
2020-09-08 11:31:29 -0400

    Added server.js, updated package.json

---
+++ server.js
@@ -0,0 +1,31 @@
+//Import Express - for server
+const express = require("express");
+
+//create PORT, 8080 for local testing, process.env.PORT for deployment on web server
+const PORT = process.env.PORT || 8080; 
+
+//instantiate app
+const app = express();
+
+//Set server to serve static content from the "public" directory
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// Set Handlebars as renderer
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+//Import routes from router and link them to the server
+const routes = require("./controllers/burgers_controller");
+app.use(routes);
+
+//Start PORT listener
+app.listen(PORT, function() {
+    //Log when server starts
+    console.log("Server listening on: http://localhost:" + PORT);
+});
